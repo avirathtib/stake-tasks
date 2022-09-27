13618b6112a9a82a36a2301ef27a8151735e52b2
avirathtib <avirathtibrewala@gmail.com>
2022-09-26 22:54:22 -0400

    connect wallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,82 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [disabled, setDisabled] = useState(true);
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+      setWalletConnected(true);
+      setDisabled(false);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+      setDisabled(false);
+      // setWalletConnected(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <ConnectButton />
+      {currentAccount === "" ? (
+        renderNotConnectedContainer()
+      ) : (
+        <p>Wallet connected with address {currentAccount}</p>
+      )}
     </div>
   );
 }
